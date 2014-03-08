837ae8ebe6b2044f84fb88379bedc7ffcc71aaf5
Andrew Akagawa <andrewakagawa@gmail.com>
2014-03-08 06:19:08 +0000

    using json for routing

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ if ('development' == app.get('env')) {
 }
 
 
-         
+var region = require('./javascripts/regions.json');         
 
 function loadRegion(req, res, next) {
     var regName = req.params.regName;
