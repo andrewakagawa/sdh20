a554cda561a65c7b1ef37da59df5590c850e92c5
Andrew Akagawa <andrewakagawa@gmail.com>
2014-04-04 07:43:12 +0000

    cleaned up everything

--- app.js
+++ app.js
@@ -49,31 +49,6 @@ app.get('/test', function(req, res){
   });
 });
 
-app.get('/region2', function(req, res){
-  res.render('region2', {
-    title: 'D3 test'
-  });
-});
-
-app.get('/test3', function(req, res){
-  res.render('test3', {
-    title: 'D3 test'
-  });
-});
-
-app.get('/test4', function(req, res){
-  res.render('test4', {
-    title: 'D3 test'
-  });
-});
-
-
-app.get('/region2', function(req, res){
-  res.render('region2', {
-    title: 'D3 test'
-  });
-});
-
 
 //create an array of
 var region = [
